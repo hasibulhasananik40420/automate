2d0e86405d086d779ef898a3f88ed0f5bcee608e
hasibulhasananik40420 <hasibulhasananik40420@gmail.com>
2024-05-26 15:27:12 +0600

    added

--- src/components/ui/CompleteProfile/CompleteProfile.jsx
+++ src/components/ui/CompleteProfile/CompleteProfile.jsx
@@ -6,7 +6,7 @@ const CompleteProfile = () => {
             <div className='flex justify-between lg:px-[150px] md:px-[100px] px-[50px] lg:pt-[26px] pt-[14px]'>
               
                 <div className='flex items-center w-full'>
-                    <div className='w-[30px] h-[30px] rounded-full md:text-[18px] text-[14px] font-semibold md:leading-[27px] leading-[21px] bg-[#E5E5E5] border-[1.5px] border-[#0C1116] flex justify-center items-center mx-auto p-2 md:p-0'>1</div>
+                    <div className='w-[30px] md:h-[30px] h-[26px] rounded-full md:text-[18px] text-[16px] font-semibold md:leading-[27px] leading-[21px] bg-[#E5E5E5] border-[2px] border-[#0C1116] flex justify-center items-center mx-auto p-[8px] md:p-0'>1</div>
                      <div className='lg:w-[460px] md:w-[300px] w-full h-[1px] bg-[#CECECE] '></div>
                     
                
